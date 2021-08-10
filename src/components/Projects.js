fc540000541831a2b7aafbcaa5d699c9f69ebe2b
mylliejohnson <mylliejohnson@gmail.com>
2021-08-09 23:56:34 -0400

    fixed links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,13 +6,13 @@ function Projects(props) {
     <div className="Projects snap" id="projects">
       <div className="proj-styles">
         <div className="project project1">
-            <h2>PLANT PARTY</h2>
+            <h2><a href="https://plant-party.netlify.com" target="_blank">PLANT PARTY</a></h2>
         </div>
         <div className="project project2">
-        <h2>IRONMAKEUP</h2>
+        <h2><a href="https://myllies-api-project.netlify.com" target="_blank">IRONMAKEUP</a></h2>
         </div>
         <div className="project project3">
-        <h2>BUBBLE POP</h2>
+        <h2><a href="https://mylliejohnson.github.io/my-bubble-game-js/index.html" target="_blank">BUBBLE POP</a></h2>
         </div>
       </div>
     </div>
